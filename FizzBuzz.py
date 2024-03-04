c873aa94df13969b628fa4f031063ef669200738
jckcob <112060475+JNTGYV@users.noreply.github.com>
2024-03-04 16:06:31 -0600

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 for i in range(1, 101):
     if i % 3 == 0 and i % 5 == 0:
         print("FizzBuzz")
@@ -7,3 +8,23 @@ for i in range(1, 101):
         print("Buzz")
     else:
         print(i)
+=======
+def fizzBuzz(minNum, MaxNum):
+    for num in range(1, 101):
+        result = ""
+        if num % 3 == 0:
+            result += "Fizz"
+        if num % 5 == 0:
+            result += "Buzz"
+    
+        print(result or num)
+
+
+
+def main():
+    fizzBuzz(1,101)
+
+
+if __name__ == "__main__":
+    main()
+>>>>>>> Stashed changes
