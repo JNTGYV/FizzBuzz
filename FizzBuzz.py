f5f86b8026e956ade1b323068b4c6484387f9a18
jckcob <112060475+JNTGYV@users.noreply.github.com>
2024-03-04 16:07:04 -0600

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,14 +1,3 @@
-<<<<<<< Updated upstream
-for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Fizz")
-    elif i % 5 == 0:
-        print("Buzz")
-    else:
-        print(i)
-=======
 def fizzBuzz(minNum, MaxNum):
     for num in range(1, 101):
         result = ""
